c8e133098b1b6d207cbd4d5319f87ca1ca9f3016
jeongjiwonz <86813428+jeongjiwonz@users.noreply.github.com>
2021-07-20 18:11:57 +0900

    jQuery 학습

---
+++ exam/mobile_slide_re.js
@@ -0,0 +1,34 @@
+
+const mobileside = {
+	//사이드 메뉴 열기
+	open : function(){
+		
+	},
+	
+	// 사이드 메뉴 닫기
+	close : function(){
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+$(function(){
+	$('#side_open,#layer_dim').click(function(){
+		mobileSide.open();
+	});
+	
+	$('#side_close,#layer_dim').click(function(){
+		mobileSide.close();
+	});
+	
+});
